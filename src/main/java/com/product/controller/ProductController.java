01875466accecaa30a95462fbc4e3ddd56488c48
bhavani <bhavanimadhikuntla313@gmail.com>
2026-08-04 17:49:59 +0530

    commited successfully

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -21,7 +21,9 @@ import com.product.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "*")
+@CrossOrigin(
+ origins="https://spring-boot-with-react-git-299951212402.us-central1.run.app"
+)
 public class ProductController {
 @Autowired
 private ProductService prodservice;
